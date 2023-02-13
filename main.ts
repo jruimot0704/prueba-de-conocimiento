95ca8beb6627c3833c87d693054bf85d7c869350
jruimot0704 <122780882+jruimot0704@users.noreply.github.com>
2023-02-13 10:31:56 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+let al_azar = 0
+input.onButtonPressed(Button.A, function () {
+    if (input.logoIsPressed()) {
+        al_azar = randint(0, 20)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
